4017de3f2d24398ad9058ca9bf2cd7b7c76a462a
unknown <prochafaa@gmail.com>
2022-03-24 22:36:07 +0100

    fin du mini project

--- app.js
+++ app.js
@@ -4,14 +4,30 @@ images.forEach(element => {
     element.addEventListener('click',function(e){
         let src=e.target.getAttribute('src');
         console.log('click')
+        let content=document.createElement('div')
+        content.className='content';
         let div=document.createElement('div')
         div.className='box';
         let image=document.createElement('img');
         image.src=src;
-
+         
         div.appendChild(image);
-        document.body.appendChild(div);
+        content.appendChild(div);
+        document.body.appendChild(content);
         
     })
     
 });
+document.body.addEventListener('click',function(e){
+   if(e.target.className === 'content'){
+       e.target.addEventListener('click',function(e){
+           e.stopPropagation();
+           console.log('click in the content');
+           e.target.classList.add('fade');
+           e.target.addEventListener('transitionend',function(){
+               e.target.parentNode.removeChild(e.target);
+           })
+
+       })
+   }
+})
